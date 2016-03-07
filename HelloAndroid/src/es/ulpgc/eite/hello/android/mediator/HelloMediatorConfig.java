00b6358bbc5535e91543584b7981a7d22e563f16
Ruben Vazquez Gonzalez <rubenvg.95@gmail.com>
2016-03-07 23:46:59 +0000

    Rotación de pantalla

--- HelloAndroid/src/es/ulpgc/eite/hello/android/mediator/HelloMediatorConfig.java
+++ HelloAndroid/src/es/ulpgc/eite/hello/android/mediator/HelloMediatorConfig.java
@@ -62,6 +62,9 @@ public class HelloMediatorConfig extends MediatorConfig{
 
         getTransitions().add(new MediatorTransition(LandscapeHelloView.class, ByeView.class, HelloMediatorCode.CLICK_HELLO));
 
+        // No puedes pasar de HellowView a las otras, sino de LandscapeHelloView o de PortraitHelloView a las otras. Creo que tiene que ver
+        //con que la clase HelloView sea abstracta, no se pueden crear objetos de dicha clase. Preguntar!
+
         //getTransitions().add(new MediatorTransition(HelloView.class, CounterView.class, HelloMediatorCode.CLICK_COUNTER));
 
         getTransitions().add(new MediatorTransition(LandscapeHelloView.class, CounterView.class, HelloMediatorCode.CLICK_COUNTER));
