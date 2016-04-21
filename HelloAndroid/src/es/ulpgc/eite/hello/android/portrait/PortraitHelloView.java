422bb10247306cf6f3f236532a8fb1ba4ed45440
Ruben Vazquez Gonzalez <rubenvg.95@gmail.com>
2016-04-21 11:32:40 +0100

    Rotación de pantalla

--- HelloAndroid/src/es/ulpgc/eite/hello/android/portrait/PortraitHelloView.java
+++ HelloAndroid/src/es/ulpgc/eite/hello/android/portrait/PortraitHelloView.java
@@ -8,7 +8,6 @@ import es.ulpgc.eite.hello.android.screen.hello.view.HelloView;
  */
 public class PortraitHelloView extends HelloView{
 
-
     @Override
     public int getLayout() {
         return R.layout.hello_view;
